2528f259f021824f40829259f81c72f4a53f380b
Choihohee <lovelove4927@naver.com>
2024-02-13 22:51:59 +0900

    add: Baekjoon10808

--- Beakjoon/Baekjoon10808.java
+++ Beakjoon/Baekjoon10808.java
@@ -1,2 +1,21 @@
-package ch01.Beakjoon;public class Baekjoon10808 {
+package ch01.Beakjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10808 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        int arr[] = new int[26];    //알파벳 갯수 저장
+
+        //아스키코드 값 사용
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            arr[c - 97]++;  //갯수 1 늘려줌
+        }
+        //출력
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
