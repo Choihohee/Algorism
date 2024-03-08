0c4af1819c00a51bc27719a73978dd4ef3a71dae
Choihohee <lovelove4927@naver.com>
2024-03-08 23:56:30 +0900

    add: Beakjoon10845

---
+++ Beakjoon/queue/Baekjoon10845.java
@@ -0,0 +1,81 @@
+package ch01.Beakjoon.queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Baekjoon10845 {
+
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            switch (command){
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+
+                case "front":
+                    sb.append(front()).append("\n");
+                    break;
+
+                case "back":
+                    sb.append(back()).append("\n");
+                    break;
+
+            }
+        }
+        System.out.println(sb);
+    }
+    public static void push(int x){
+        queue.add(x);
+    }
+    public static int pop(){
+        if (queue.isEmpty())
+            return -1;
+        else
+            return queue.poll();
+    }
+    public static int size(){
+        return queue.size();
+    }
+    public static int empty(){
+        if (queue.isEmpty())
+            return 1;
+        else
+            return 0;
+    }
+    public static int front(){
+        if (queue.isEmpty())
+            return -1;
+        else
+            return queue.peek();
+    }
+    public static int back(){
+        if (queue.isEmpty())
+            return -1;
+        else
+            return ((LinkedList<Integer>)queue).getLast();
+    }
+}
