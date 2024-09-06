8f321b44de76be3bb259450af8b9a3ecf0b0ffca
Choihohee <lovelove4927@naver.com>
2024-09-06 12:48:05 +0900

    Baekjoon2018

---
+++ Beakjoon/TwoPointer/Baekjoon2018.java
@@ -0,0 +1,30 @@
+package ch01.Beakjoon.TwoPointer;
+
+import java.util.Scanner;
+
+public class Baekjoon2018 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        int sum = 1;
+        int start = 1;
+        int end = 1;
+        int count = 1;
+
+        while (start!=input){
+
+            if(sum < input){
+                end++;
+                sum += end;
+            } else if (sum > input) {
+                sum -= start;
+                start++;
+            }else {
+                count++;
+                end++;
+                sum += end;
+            }
+        }
+        System.out.println(count);
+}
