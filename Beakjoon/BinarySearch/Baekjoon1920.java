c0d5c9a07c8a1fec00214bfd3dad9bd73b4e1cb8
Choihohee <lovelove4927@naver.com>
2024-09-09 17:11:53 +0900

    Baekjoon1920

---
+++ Beakjoon/BinarySearch/Baekjoon1920.java
@@ -0,0 +1,36 @@
+package ch01.Beakjoon.BinarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            int key = Arrays.binarySearch(arr, num);
+
+            if(key < 0)
+                sb.append(0 + "\n");
+            else
+                sb.append(1 + "\n");
+        }
+        System.out.println(sb);
+    }
+}
